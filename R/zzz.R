3666c67c5c9a1cdc1b274088948528aa6b958a35
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-05-03 16:22:16 -0400

    load the 'utils' package when attached for the 'Sweave' function

--- R/zzz.R
+++ R/zzz.R
@@ -1,11 +1,16 @@
 .onLoad <- function(lib, pkg) {
-    pkgList <- c("filehash", "stashR")
-    
-    for(pkg in pkgList) {
-        if(!require(pkg, quietly = TRUE, character.only = TRUE))
-            stop(gettextf("'%s' package required", pkg))
-    }
-    assign("cacheDir", ".", .cacheEnv)
+        pkgList <- c("filehash", "stashR")
+        
+        for(pkg in pkgList) {
+                if(!require(pkg, quietly = TRUE, character.only = TRUE))
+                        stop(gettextf("'%s' package required", pkg))
+        }
+        assign("cacheDir", ".", .cacheEnv)
+}
+
+.onAttach <- function(lib, pkg) {
+        if(!require(utils))
+                stop("'utils' package required to use 'Sweave'")
 }
 
 .cacheEnv <- new.env(parent = emptyenv())
