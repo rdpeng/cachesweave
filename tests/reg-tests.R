34813f8d18ec61049b5da34809bcad97d8662ff6
Roger D. Peng [powerbook] <rpeng@jhsph.edu>
2007-05-09 21:50:16 -0400

    Expand tests to make sure that objects are cached correctly
    After Sweave-ing the sample file, we load the stashR database
    directly to check if the 'airquality' and 'fit' objects are
    cached

--- tests/reg-tests.R
+++ tests/reg-tests.R
@@ -5,3 +5,14 @@ infile <- system.file("example", "simpleRR.Rnw", package = "cacheSweave")
 setCacheDir("cache")
 
 Sweave(infile, driver = cacheSweaveDriver)
+
+ls()
+
+
+## Check to see that objects were properly cached
+## Database should contain 'airquality' and 'fit'
+
+dname <- dir("cache", full.names = TRUE)
+library(stashR)
+db <- new("localDB", dir = dname, name = "cacheDB")
+dbList(db)
