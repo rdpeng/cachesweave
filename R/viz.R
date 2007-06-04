c7ae6fc3c9681d5398b6d4b46c4ad86dc14e2d3f
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-06-04 17:26:00 -0400

    Add 'showcache' function which returns a list of objects in the cache

--- R/viz.R
+++ R/viz.R
@@ -1,5 +1,42 @@
-viewCache <- function(cachedir = NULL) {
+showcache <- function(cachedir = NULL) {
         if(is.null(cachedir))
                 cachedir <- getCacheDir()
-        
+        dirs <- dir(cachedir, full.names = TRUE)
+
+        ## If 'cachedir' is not "." then we assume that everything in
+        ## 'cachedir' is a cache database directory
+
+        if(cachedir == ".") {
+                use <- sapply(basename(dirs), isCacheDir)
+                dirs <- dirs[use]
+        }
+        if(length(dirs) == 0)
+                return(NULL)
+        cacheInfo <- lapply(dirs, function(cdir) {
+                exprFiles <- dir(cdir, full.names = TRUE)
+                keys <- readKeys(exprFiles)
+                list(dir = basename(cdir), keys = keys)
+        })
+        names(cacheInfo) <- chunkName(basename(dirs))
+        cacheInfo
+}
+
+chunkName <- function(x) {
+        sub("_\\S+$", "", x, perl = TRUE)
+}
+
+readKeys <- function(exprFiles) {
+        keys <- lapply(exprFiles, function(f) {
+                index <- getIndex(f)
+                names(index)
+        })
+        unique(unlist(keys))
+}
+
+
+## Heuristic: [something]_[something] is a cache database directory
+
+isCacheDir <- function(name) {
+        v <- grep("\\S+_\\S+", name, perl = TRUE)
+        length(v) > 0
 }
