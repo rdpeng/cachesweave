275140e71ac5ddd06ec8b8bc84575729c5c253da
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-06-04 17:25:10 -0400

    Very simple code analyzer to build dependency trees
    'buildDepTree' takes an integer into an expression list and finds
    (recursively) all of the previous expressions on which the current
    expression depends.  So you can do something like
    
    e <- getExpressions(Rfile)
    edep <- lapply(e, findExprLocals)
    eobj <- lapply(e, exprObjects)
    buildDepTree(11, e, edep, eobj)
    
    which finds all the dependencies for expression number 11.

---
+++ R/lexical.R
@@ -0,0 +1,66 @@
+getExpressions <- function(file) {
+        fullpath <- normalizePath(file)
+        tmpdir <- tempdir()
+        wd <- getwd()
+        on.exit(setwd(wd))
+
+        setwd(tmpdir)
+        output <- "StangleTmp.R"
+        Stangle(fullpath, output = output, split = FALSE, quiet = TRUE)
+        parse(output)
+}
+
+findExprGlobals <- function(expr) {
+        f <- function() {}
+        body(f) <- expr
+        findGlobals(f)
+}
+
+findExprLocals <- function(expr) {
+        globals <- findExprGlobals(expr)
+        isLocal <- sapply(globals, function(sym) {
+                pos <- find(sym)
+                length(pos) == 0
+        })
+        globals[isLocal]
+}
+
+
+matchList <- function(x, listTab) {
+        m <- sapply(listTab, function(tab) match(x, tab))
+        if(all(is.na(m)))
+                NA
+        else
+                which(!is.na(m))[1]
+}
+
+buildDepTree <- function(i, exprList, edep, eobj) {
+        if(is.na(i) || i < 2)
+                return(as.integer(NA))
+        exprList <- exprList[1:i]
+        edep <- edep[1:i]
+        eobj <- eobj[1:i]
+
+        if(length(edep[[i]]) == 0)  ## no dependencies
+                return(as.integer(NA))
+        created <- lapply(edep[[i]], function(x) {
+                as.integer( i - matchList(x, eobj[(i-1):1]) )
+        })
+        names(created) <- edep[[i]]
+        lapply(created, function(j) {
+                list(origin = j,
+                     depends = buildDepTree(j, exprList, edep, eobj))
+        })
+}
+
+################################################################################
+
+## In order to figure out what objects an expression gives rise to, we
+## have to evaluate it.
+
+exprObjects <- function(expr) {
+        env <- evalAndCache(expr, exprFile = NULL, cache = FALSE)
+        keys <- ls(env, all.names = TRUE)
+        copy2env(keys, env, globalenv())
+        keys
+}
