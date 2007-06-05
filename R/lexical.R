1f09dd15ed19a03253bbdca6896dc6fc0755fe37
Roger D. Peng [audrey] <rpeng@jhsph.edu>
2007-06-05 08:33:50 -0400

    Clean up return values of buildDepTree

--- R/lexical.R
+++ R/lexical.R
@@ -7,7 +7,7 @@ getExpressions <- function(file) {
         setwd(tmpdir)
         output <- "StangleTmp.R"
         Stangle(fullpath, output = output, split = FALSE, quiet = TRUE)
-        parse(output)
+        parse(output, srcfile = NULL)
 }
 
 findExprGlobals <- function(expr) {
@@ -36,22 +36,45 @@ matchList <- function(x, listTab) {
 
 buildDepTree <- function(i, exprList, edep, eobj) {
         if(is.na(i) || i < 2)
-                return(as.integer(NA))
+                return(list(origin = NA, origin.idx = NA, depends = NA))
         exprList <- exprList[1:i]
         edep <- edep[1:i]
         eobj <- eobj[1:i]
 
         if(length(edep[[i]]) == 0)  ## no dependencies
-                return(as.integer(NA))
+                return(list(origin = NA, origin.idx = NA, depends = NA))
         created <- lapply(edep[[i]], function(x) {
                 as.integer( i - matchList(x, eobj[(i-1):1]) )
         })
         names(created) <- edep[[i]]
         lapply(created, function(j) {
-                list(origin = exprList[j],
-                     origin.idx = j,
-                     depends = buildDepTree(j, exprList, edep, eobj))
+                if(is.na(j)) 
+                        list(origin = NA,
+                             origin.idx = NA,
+                             depends = NA)
+                else
+                        list(origin = exprList[j],
+                             origin.idx = j,
+                             depends = buildDepTree(j, exprList, edep, eobj))
         })
+}
+
+runTree <- function(exprList) {
+        with(exprList, {
+                lapply(seq_along(expr), function(i) {
+                        buildDepTree(i, expr, dep, obj)
+                })
+        })
+}
+
+runFile <- function(file) {
+        message("getting expressions")
+        e <- getExpressions(file)
+        message("finding expression locals")
+        edep <- lapply(e, findExprLocals)
+        message("getting expression objects")
+        eobj <- lapply(e, getExprObjects)
+        list(expr = e, dep = edep, obj = eobj)
 }
 
 ################################################################################
@@ -59,7 +82,7 @@ buildDepTree <- function(i, exprList, edep, eobj) {
 ## In order to figure out what objects an expression gives rise to, we
 ## have to evaluate it.
 
-exprObjects <- function(expr) {
+getExprObjects <- function(expr) {
         env <- evalAndCache(expr, exprFile = NULL, cache = FALSE)
         keys <- ls(env, all.names = TRUE)
         copy2env(keys, env, globalenv())
