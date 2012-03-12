cb4202a5d7dbcfa5cb94b20a941c846e440e4c2e
Roger D. Peng [amelia] <rdpeng@gmail.com>
2012-03-12 10:06:37 -0400

    Don't load 'utils' in .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -2,9 +2,4 @@
         assign("cacheDir", ".", .cacheEnv)
 }
 
-.onAttach <- function(lib, pkg) {
-        if(!require(utils))
-                stop("'utils' package required to use 'Sweave'")
-}
-
 .cacheEnv <- new.env(parent = emptyenv())
